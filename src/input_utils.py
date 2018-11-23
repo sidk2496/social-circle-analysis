71958fccda7be30fa2c7cd3669691118a2220821
vignesh2496 <vignesh2496@gmail.com>
2018-11-23 17:27:27 -0500

    starting union find

--- src/input_utils.py
+++ src/input_utils.py
@@ -1,37 +1,60 @@
 import numpy as np
-from graph_utils import Node
+from graph_utils import Node, Edge
+import pickle
 import os
 
 def parse_feat_file(filename):
-    with open(filename, 'r') as file:
-        lines = file.read().splitlines()
-        nodes = []
-        for line in lines:
+    with open(data_dir + filename, 'r') as file:
+        nodes = {}
+        for line in file:
+            line = line.rstrip()
             tokens = line.split(' ')
             idx = int(tokens[0])
             attributes = list(map(int, tokens[1: ]))
-            nodes.append(Node(idx, attributes))
+            nodes[idx] = Node(idx, attributes, [idx])
     return nodes
 
-def parse_edge_file(filename):
-    with open(filename, 'r') as file:
-        lines = file.read().splitlines()
-        for line in lines:
+def parse_edge_file(filename, nodes):
+    with open(data_dir + filename, 'r') as file:
+        edges = []
+        for line in file:
+            line = line.rstrip()
             tokens = line.split(' ')
-            u =
+            u = int(tokens[0])
+            v = int(tokens[1])
+            edges.append(Edge(nodes[u], nodes[v]))
+    return edges
 
-data_dir = '../../facebook/'
+data_dir = '../../data/facebook/'
 filenames = os.listdir(data_dir)
 
-feat_files = [filename for filename in filenames if '.feat' in filename]
+feat_files = [filename for filename in filenames if '.feat' in filename and 'names' not in filename]
 egofeat_files = [filename for filename in filenames if '.egofeat' in filename]
 edge_files = [filename for filename in filenames if '.edges' in filename]
 
-nodes = {}
+datasets = {}
 for filename in feat_files:
-    nodes[filename.split('.')[0]] = parse_feat_file(filename)
+    ego_idx = int(filename.split('.')[0])
+    dataset = {}
+    dataset['ego_idx'] = ego_idx
+    dataset['nodes'] = parse_feat_file(filename)
+    datasets[ego_idx] = dataset
+    
+for filename in egofeat_files:
+    ego_idx = int(filename.split('.')[0])
+    with open(data_dir + filename, 'r') as file:
+        line = file.readline()
+        line = line.rstrip()
+        tokens = line.split(' ')
+        attributes = list(map(int, tokens))
+    datasets[ego_idx]['nodes'][ego_idx] = Node(ego_idx, attributes, [idx])
 
-nodes = {}
-for filename in feat_files:
-    nodes[filename.split('.')[0]] = parse_feat_file(filename)
+for filename in edge_files:
+    ego_idx = int(filename.split('.')[0])
+    datasets[ego_idx]['edges'] = parse_edge_file(filename, datasets[ego_idx]['nodes'])
 
+count_dataset = 1
+for _, dataset in datasets.items():
+    with open(data_dir + 'processed/data_' + str(count_dataset) + '.pkl', 'wb') as data_file:
+        pickle.dump(dataset, data_file)
+    count_dataset += 1
