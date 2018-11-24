d496836f1e810639ded233a4a2852f037d5adf31
sidk2496 <sid2496@gmail.com>
2018-11-24 11:57:30 -0500

    added circle formation code

--- src/graph_utils.py
+++ src/graph_utils.py
@@ -19,4 +19,48 @@ class Edge:
         self.v = v
         self.w = similarity(u, v)
 
+def randomized_add(circle, v):
+    similarities = map(lambda x: similarity(v, x), circle.nodes)
+    avg_similarity = sum(similarities) / len(similarities)
+
+
+def union(u, v, circles):
+    u_diff_v = u.membership.difference(v.membership)
+    if len(u_diff_v) != 0:
+        for circle_idx in u_diff_v:
+            randomized_add(circles[circle_idx], v)
+
+    v_diff_u = u.membership.difference(v.membership)
+    if len(v_diff_u) != 0:
+        for circle_idx in v_diff_u:
+            randomized_add(circles[circle_idx], u)
+
+
+def circle_formation(nodes, edges, circles):
+    for edge in edges:
+        union(edge.u, edge.v, circles)
+    dissolve_circles(nodes, circles)
+
+def label_propagation(nodes, edges, circles):
+
+
+def IoU_score(circle1, circle2):
+    I = len(circle1.nodes.intersection(circle2.nodes))
+    U = len(circle1.nodes.union(circle2.nodes))
+    return I / U
+
+def dissolve_circles(nodes, circles):
+    circle_ids = circles.keys()
+    for i, idx1 in enumerate(circle_ids):
+        for idx2 in circle_ids[i + 1: ]:
+            iou = IoU_score(circles[idx1], circles[idx2])
+            if iou > threshold:
+                for node_idx in circles[idx2].nodes:
+                    nodes[node_idx].membership.remove(idx2)
+                del circles[idx2]
+
+
+
+
+
 
--- src/union_find.py
+++ src/union_find.py
@@ -1,5 +1,5 @@
-import pickle
-
-file_count = 2
-with open('../../data/facebook/processed/data_' + str(file_count) + '.pkl', 'rb') as data_file:
-	data = pickle.load(data_file)
+# import pickle
+#
+# file_count = 2
+# with open('../../data/facebook/processed/data_' + str(file_count) + '.pkl', 'rb') as data_file:
+# 	data = pickle.load(data_file)
